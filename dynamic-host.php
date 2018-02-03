8be6f1f6b6d951e810bb49972de335eab0020d47
Val Catalasan <vcatalasan@gmail.com>
2018-02-03 15:29:21 -0800

    replaced localhost with dynamic host name

--- dynamic-host.php
+++ dynamic-host.php
@@ -6,10 +6,12 @@ Version: 1.0.0
 Author: Val Catalasan
 */
 
+define( 'DYNAMIC_HOST', isset($_SERVER['HTTP_X_ORIGINAL_HOST']) ? $_SERVER['HTTP_X_ORIGINAL_HOST'] : $_SERVER['HTTP_HOST'] );
+
 function dynamic_url($url) {
 	$result = parse_url( $url );
 	$scheme = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : $result['scheme'];
-	$host   = isset($_SERVER['HTTP_X_ORIGINAL_HOST']) ? $_SERVER['HTTP_X_ORIGINAL_HOST'] : $_SERVER['HTTP_HOST'];
+	$host   = DYNAMIC_HOST; //isset($_SERVER['HTTP_X_ORIGINAL_HOST']) ? $_SERVER['HTTP_X_ORIGINAL_HOST'] : $_SERVER['HTTP_HOST'];
 	$path = isset($result['path']) ? $result['path'] : '';
 	$url = $scheme . '://' . $host . $path;
 	return $url;
@@ -25,7 +27,27 @@ foreach( [ 'post', 'page', 'attachment', 'post_type' ] as $type )
 	}, PHP_INT_MAX, 4 );
 }
 
-add_filter( 'wpse_link', function(  $url )
-{
-	return dynamic_url($url);
-}, 10, 1);
+add_filter( 'wpse_link', 'dynamic_url');
+
+add_filter( 'bloginfo', function($output, $show){
+	return preg_match('/url|home/', $show) ? dynamic_url($output) : $output;
+}, 10, 2);
+
+add_filter( 'get_blogs_of_user', function($sites, $user_id, $all){
+	return sites.array_map(function($site){
+		$site->blogname = dynamic_url($site->blogname);
+		$site->siteurl = dynamic_url($site->siteurl);
+		return $site;
+	});
+}, 10, 3);
+
+add_action( 'init', function() {
+    ob_start(function($buffer){
+        preg_match('/(.+):/', DYNAMIC_HOST, $host);
+        return str_replace('localhost', $host[1], $buffer);
+    });
+});
+
+add_action( 'shutdown', function() {
+    ob_end_flush();
+});
