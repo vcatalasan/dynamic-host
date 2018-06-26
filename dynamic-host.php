c195b1831a6bbb08781f2e29ce89f5537cc2a673
Val Catalasan <val@bscmanage.com>
2018-06-26 11:03:21 -0700

    fixed issue with login and page redirect

--- dynamic-host.php
+++ dynamic-host.php
@@ -2,11 +2,12 @@
 /*
 Plugin Name: Dynamic Host
 Description: Allows Wordpress to run anywhere i.e. local, development, or production.
-Version: 1.0.1
+Version: 1.0.2
 Author: Val Catalasan
 */
 
 define( 'DYNAMIC_HOST', isset($_SERVER['HTTP_X_ORIGINAL_HOST']) ? $_SERVER['HTTP_X_ORIGINAL_HOST'] : $_SERVER['HTTP_HOST'] );
+define( 'SITE_URL', get_option('siteurl'));
 
 function dynamic_url($url) {
 	$result = parse_url( $url );
@@ -17,35 +18,18 @@ function dynamic_url($url) {
 	return $url;
 }
 
-add_filter('option_siteurl', 'dynamic_url');
+add_filter( 'login_redirect', function( $url, $query, $user ) {
+	return dynamic_url($url);
+}, 10, 3 );
 
-foreach( [ 'post', 'page', 'attachment', 'post_type' ] as $type )
-{
-	add_filter( $type . '_link', function ( $url, $a1 = null, $a2 = null, $a3 = null ) use ( $type )
-	{
-		return apply_filters( 'wpse_link', $url );
-	}, PHP_INT_MAX, 4 );
-}
-
-add_filter( 'wpse_link', 'dynamic_url');
-
-add_filter( 'bloginfo', function($output, $show){
-	return preg_match('/url|home/', $show) ? dynamic_url($output) : $output;
-}, 10, 2);
-
-add_filter( 'get_blogs_of_user', function($sites, $user_id, $all){
-	return sites.array_map(function($site){
-		$site->blogname = dynamic_url($site->blogname);
-		$site->siteurl = dynamic_url($site->siteurl);
-		return $site;
-	});
-}, 10, 3);
+add_filter( 'wp_redirect', function( $location, $status ){
+	return dynamic_url($location);
+});
 
 add_action( 'init', function() {
     ob_start(function($buffer){
-        $site_url = get_option('site_url');
-        $dynamic_url = dynamic_url(DYNAMIC_HOST);
-	    $output = str_replace($site_url, $dynamic_url, $buffer);
+        $dynamic_url = dynamic_url(SITE_URL);
+	    $output = str_replace(SITE_URL, $dynamic_url, $buffer);
         return $output;
     });
 });
